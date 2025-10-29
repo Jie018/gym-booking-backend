73c7c59f58fff82355e6baffc4b0f61cc93922c7
Jie018 <cynthia381@gmail.com>
2025-10-29 16:01:23 +0800

    forth backend

--- database.py
+++ database.py
@@ -1,12 +1,28 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Aa001!school@localhost:3306/gym_booking"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Aa001!school@localhost:3306/gym_booking"
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:Aa001!school@localhost:3306/gym_booking")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
